a5a3b009de5050bf9cb9ad0e076a5360fcf3e931
Ramtane Hidja <ramtane.hidja@smile.fr>
2025-04-04 14:24:39 +0200

    Filters: use class static method - way 2

---
+++ exemples/filter/static_method_2.php
@@ -0,0 +1,20 @@
+<?php
+
+include __DIR__.'/../../vendor/autoload.php';
+
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+use Twig\TwigFilter;
+
+$loader = new FilesystemLoader('./templates');
+$twig = new Environment($loader, [
+    'cache' => './caches',
+    'auto_reload' => true,
+]);
+
+// or a class static method
+$filter = new TwigFilter('rot_13', 'Rhidja\\Twig\\Filter\\RotFilter::rot13Filter');
+
+$twig->addFilter($filter);
+
+echo $twig->render('filter/rot_13.html.twig');
