1ccb634b82ddce95c529e46026221d1dc2311a18
Ramtane Hidja <ramtane.hidja@smile.fr>
2025-06-05 13:10:27 +0200

    Refactoring

--- exemples/app.php
+++ exemples/app.php
@@ -4,9 +4,13 @@ include __DIR__.'/../vendor/autoload.php';
 
 use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
+use Twig\Extension\DebugExtension;
 
 $loader = new FilesystemLoader('./../../templates');
 $twig = new Environment($loader, [
     'cache' => './../../caches',
     'auto_reload' => true,
+    'debug' => true,
 ]);
+
+$twig->addExtension(new DebugExtension());
--- exemples/function/block.php
+++ exemples/function/block.php
@@ -4,8 +4,4 @@ $twig = null;
 
 include __DIR__ . '/../app.php';
 
-use Twig\Extension\DebugExtension;
-
-$twig->addExtension(new DebugExtension());
-
 echo $twig->render('function/block.html.twig');
